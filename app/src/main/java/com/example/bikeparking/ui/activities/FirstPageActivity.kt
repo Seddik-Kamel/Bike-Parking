26c73c544ee4602f493dad9988b2f9ed4b178f00
KS <seddikkamel1@gmail.com>
2021-10-12 00:00:19 +0200

    Refactoring name.

--- app/src/main/java/com/example/bikeparking/ui/activities/FirstPageActivity.kt
+++ app/src/main/java/com/example/bikeparking/ui/activities/FirstPageActivity.kt
@@ -1,9 +1,10 @@
-package com.example.bikeparking
+package com.example.bikeparking.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bikeparking.R
 
-class MainActivity : AppCompatActivity() {
+class FirstPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_page)
