258a06e3e2e59d1922e0c2d93f7765736bc8e747
KS <seddikkamel1@gmail.com>
2021-10-12 00:23:16 +0200

    Refactoring : Rename

--- app/src/main/java/com/example/bikeparking/ui/activities/SignUpEmailActivity.kt
+++ app/src/main/java/com/example/bikeparking/ui/activities/SignUpEmailActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bikeparking.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpEmailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_sign_up_email)
     }
 }
